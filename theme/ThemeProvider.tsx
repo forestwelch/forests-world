0507f5be72807f7116317e8f482475b5e9ee3e01
Nick Welch <nwelchr@gmail.com>
2024-12-18 11:43:11 -0800

    Update provider to use local storage + change html component

--- theme/ThemeProvider.tsx
+++ theme/ThemeProvider.tsx
@@ -1,27 +1,41 @@
 "use client";
 
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const ThemeContext = createContext<
   { theme: string; toggleTheme: () => void } | undefined
 >(undefined);
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState<string | null>(null);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+      setTheme("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      setTheme("light");
+    }
+  }, []);
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    const newTheme = document.documentElement.classList.contains("dark")
+      ? "light"
+      : "dark";
+    document.documentElement.classList.toggle("dark");
+    localStorage.setItem("theme", newTheme);
+    setTheme(newTheme);
   };
+
+  if (theme === null) {
+    return null;
+  }
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <div
-        className={`fixed top-0 left-0 right-0 bottom-0 z-[-1] ${
-          theme === "dark" ? "bg-gradient-dark" : "bg-gradient-light"
-        }`}
-      >
-        {children}
-      </div>
+      {children}
     </ThemeContext.Provider>
   );
 };

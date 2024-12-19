49c9e9eacd0b98ba73ce23ba886bfde9f4eebb43
Nick Welch <nwelchr@gmail.com>
2024-12-18 16:30:18 -0800

    Update styles for about age

--- components/About.tsx
+++ components/About.tsx
@@ -1,30 +1,33 @@
-"use client";
-
-import Image from "next/image";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import Image from "next/image";
 
 const About = () => {
   const { t } = useTranslation();
 
   return (
-    <div>
-      <h1 className="text-center font-thin text-5xl tracking-tight px-[5%] sm:text-3xl sm:px-[20%]">
-        {t("Welcome")} {t("World")}.
+    <section className="p-20 flex flex-col gap-8">
+      <h1 className="text-6xl font-thin text-center md:hidden">
+        {t("WelcomeShort")}
       </h1>
-      <div className="flex flex-row items-center justify-around sm:flex-col-reverse">
-        <section className="flex max-w-[600px] p-2 sm:max-w-[90%] sm:p-1 sm:text-center">
-          <p>{t("Blurb")}</p>
+      <h1 className="text-6xl lg:text-8xl font-thin text-center hidden md:block">
+        {t("WelcomeFull")}
+      </h1>
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-center gap-8">
+        <section className="max-w-xl">
+          <p className="text-justify">{t("Blurb")}</p>
         </section>
-        <Image
-          className="rounded-full object-cover border border-dotted border-gray-500 p-0.5"
-          width={300}
-          height={300}
-          src="/assets/images/self.webp"
-          alt="Self"
-        />
+        <div className="">
+          <Image
+            className="max-w-64 max-h-64 rounded-full border border-dotted border-gray-400 p-1 object-cover"
+            src="/assets/images/self.webp"
+            alt="Self"
+            width={300}
+            height={300}
+          />
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 

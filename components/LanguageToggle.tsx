d0d4a11d3d893def19f7e22822f9ea727147ab23
Nick Welch <nwelchr@gmail.com>
2024-12-18 18:21:37 -0800

    Move i18n files, make it work

--- components/LanguageToggle.tsx
+++ components/LanguageToggle.tsx
@@ -5,24 +5,37 @@ const LanguageToggle = () => {
   const { i18n } = useTranslation();
   const [visible, setVisible] = useState(false);
 
+  const languages = [
+    "en",
+    "es",
+    "pt",
+    "fr",
+    "de",
+    "ru",
+    "ja",
+    "ko",
+    "zh",
+    "hi",
+    "ar",
+  ];
+  const currentLanguageIndex = languages.indexOf(i18n.language);
+
   useEffect(() => {
     setVisible(true);
   }, [i18n.language]);
 
-  const changeLanguage = (lng: string) => {
-    i18n.changeLanguage(lng);
+  const changeLanguage = () => {
+    const nextLanguageIndex = (currentLanguageIndex + 1) % languages.length;
+    i18n.changeLanguage(languages[nextLanguageIndex]);
   };
-
-  const fromLanguage = i18n.language;
-  const toLanguage = fromLanguage === "en" ? "fr" : "en";
 
   return (
     <a
       className="language-toggle inline-block p-1.5 m-2 text-md font-light relative transition-opacity duration-300"
       style={{ opacity: visible ? 1 : 0 }}
-      onClick={() => changeLanguage(toLanguage)}
+      onClick={changeLanguage}
     >
-      {fromLanguage.toUpperCase()}
+      {i18n.language.toUpperCase()}
     </a>
   );
 };
--- i18n/config.ts
+++ i18n/config.ts
@@ -1,12 +1,30 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import en from "./en.json";
-import fr from "./fr.json";
+import en from "./lang/en.json";
+import fr from "./lang/fr.json";
+import de from "./lang/de.json";
+import ja from "./lang/ja.json";
+import ko from "./lang/ko.json";
+import es from "./lang/es.json";
+import zh from "./lang/zh.json";
+import hi from "./lang/hi.json";
+import ar from "./lang/ar.json";
+import ru from "./lang/ru.json";
+import pt from "./lang/pt.json";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     fr: { translation: fr },
+    de: { translation: de },
+    ja: { translation: ja },
+    ko: { translation: ko },
+    es: { translation: es },
+    zh: { translation: zh },
+    hi: { translation: hi },
+    ar: { translation: ar },
+    ru: { translation: ru },
+    pt: { translation: pt },
   },
   lng: "en",
   keySeparator: false,

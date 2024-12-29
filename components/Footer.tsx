8c33d42669710eb05965481133711ef5b438563b
Nick Welch <nwelchr@gmail.com>
2024-12-28 22:18:36 -0800

    Theme mode copy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer className="text-center flex flex-col gap-y-2 pb-12">
       <p>
-        You&apos;ve been viewing this site in{" "}
+        You&apos;re viewing this in{" "}
         <span className="p-[2px] rounded-lg font-bold bg-gray-200 text-gray-800 dark:bg-gray-800 dark:text-gray-200 transition duration-150 ease-in-out inline-block w-12 text-center">
           <Sparkles
             color={
@@ -24,7 +24,7 @@ const Footer = () => {
         className="font-bold hover:text-teal-700 dark:hover:text-violet-300 transition-color duration-150 ease-in-out"
         onClick={toggleTheme}
       >
-        Click here to change to{" "}
+        Click here to switch to{" "}
         <span className="p-[2px] rounded-lg font-bold bg-gray-800 text-gray-200 dark:bg-gray-200 dark:text-gray-800 transition duration-150 ease-in-out inline-block w-12 text-center">
           <Sparkles
             color={

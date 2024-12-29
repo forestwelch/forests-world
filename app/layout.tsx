23d5bee640468a819a8251c4944425be9d2f7522
Nick Welch <nwelchr@gmail.com>
2024-12-28 23:17:11 -0800

    new logo, metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,39 @@ import Providers from "@/components/Providers";
 
 export const metadata: Metadata = {
   title: "Forest's World",
-  description: "Hire me to build your next project!",
+  description:
+    "I’m a full-stack developer who believes in bridging ideas and technology for real impact—let’s build something beautiful together.",
+  openGraph: {
+    title: "Forest's World",
+    description:
+      "I’m a full-stack developer who believes in bridging ideas and technology for real impact—let’s build something beautiful together.",
+    url: "https://www.forestsworld.me",
+    siteName: "Forest's World",
+    images: [
+      {
+        url: "/assets/images/open-graph.png",
+        width: 800,
+        height: 600,
+        alt: "Forest's World Open Graph Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Forest's World",
+    description:
+      "I’m a full-stack developer who believes in bridging ideas and technology for real impact—let’s build something beautiful together.",
+    images: ["/assets/images/open-graph.png"],
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html className="scroll-smooth">
       <body>

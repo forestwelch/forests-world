9097cebe1498786bbe1dff2dc40ced7612e5f90d
Nick Welch <nwelchr@gmail.com>
2024-12-30 14:11:11 -0800

    Credit in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Sparkles from "./Sparkles";
 import { useTheme } from "@/theme/ThemeProvider";
+import Link from "next/link";
 
 const Footer = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <footer className="text-center flex flex-col gap-y-2 pb-12">
-      <p>
+    <footer className="flex flex-col gap-y-12 pb-12">
+      <p className="text-center">
         You&apos;re viewing this in{" "}
         <span className="p-[2px] rounded-lg font-bold bg-gray-200 text-gray-800 dark:bg-gray-800 dark:text-gray-200 transition duration-150 ease-in-out inline-block w-12 text-center">
           <Sparkles
@@ -18,24 +19,38 @@ const Footer = () => {
             {theme}
           </Sparkles>
         </span>{" "}
-        mode.
+        mode.{" "}
+        <button
+          className="font-bold hover:text-teal-700 dark:hover:text-violet-300 transition-color duration-150 ease-in-out"
+          onClick={toggleTheme}
+        >
+          Click here to switch to{" "}
+          <span className="p-[2px] rounded-lg font-bold bg-gray-800 text-gray-200 dark:bg-gray-200 dark:text-gray-800 transition duration-150 ease-in-out inline-block w-12 text-center">
+            <Sparkles
+              color={
+                theme === "dark" ? "hsl(252, 56%, 26%)" : "hsl(252, 62%, 70%)"
+              }
+            >
+              {theme === "dark" ? "light" : "dark"}
+            </Sparkles>
+          </span>{" "}
+          mode.
+        </button>
       </p>
-      <button
-        className="font-bold hover:text-teal-700 dark:hover:text-violet-300 transition-color duration-150 ease-in-out"
-        onClick={toggleTheme}
-      >
-        Click here to switch to{" "}
-        <span className="p-[2px] rounded-lg font-bold bg-gray-800 text-gray-200 dark:bg-gray-200 dark:text-gray-800 transition duration-150 ease-in-out inline-block w-12 text-center">
-          <Sparkles
-            color={
-              theme === "dark" ? "hsl(252, 56%, 26%)" : "hsl(252, 62%, 70%)"
-            }
+
+      <div className="max-w-md">
+        <span className="opacity-70 text-sm">
+          My site’s design draws inspiration from the lovely{" "}
+          <Link
+            className="font-semibold hover:text-violet-700 dark:hover:text-teal-400 transition-all duration-150"
+            href="https://brittanychiang.com/"
           >
-            {theme === "dark" ? "light" : "dark"}
-          </Sparkles>
-        </span>{" "}
-        mode.
-      </button>
+            portfolio of Brittany Chiang
+          </Link>
+          . However, I coded it from scratch and will continue to make it
+          uniquely mine. Thank you Brittany!
+        </span>
+      </div>
     </footer>
   );
 };

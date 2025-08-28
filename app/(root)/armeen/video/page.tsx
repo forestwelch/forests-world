4c425356dc663d3fef5066ee8c061d52f8e18b05
Nick Welch <nwelchr@gmail.com>
2025-08-28 09:52:57 -0700

    fix errors

--- app/(root)/armeen/video/page.tsx
+++ app/(root)/armeen/video/page.tsx
@@ -40,10 +40,9 @@ export default function ArmeenPage() {
               height="315"
               src="https://www.youtube.com/embed/1r346-saBQc?si=5nWOc6Sh74yaRvbd"
               title="YouTube video player"
-              frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
             ></iframe>
           </div>
           <br />

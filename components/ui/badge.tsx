5b8dce50f008c1089713ad9c834d32eeafecd0a5
Nick Welch <nwelchr@gmail.com>
2025-12-27 15:45:30 -0800

    tweak badge

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Badge = ({ term }: { term: string }) => {
   return (
-    <li className="inline-block font-medium badge rounded-xl text-sm px-2 py-1 transition-bg duration-150 ease-in-out text-light-900 bg-light-200/50 dark:text-dark-200 dark:bg-dark-900/50 border border-light-900/20 dark:border-dark-200/20">
+    <li className="inline-block font-medium badge rounded-2xl text-sm px-2 py-0.5 transition-bg duration-150 ease-in-out text-light-900 bg-light-200/50 dark:text-dark-200 dark:bg-dark-900/50 border border-light-900/20 dark:border-dark-200/20">
       {term}
     </li>
   );
